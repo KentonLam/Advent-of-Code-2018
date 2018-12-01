21f079c73361e7b5dd6b5ef0cce9058c0de113e6
Kenton Lam <kenton_lam@outlook.com>
2018-12-01 22:50:03 +1000

    Sovle first part of day 1.

--- 01_solve.py
+++ 01_solve.py
@@ -1,1 +1,10 @@
-print(1)
+
+def solve_1(numbers):
+    return sum(numbers)
+
+def solve_2(numbers):
+    pass
+
+if __name__ == "__main__":
+    with open('01_input.txt') as f:
+        print(solve_1(map(int, f.readlines())))
