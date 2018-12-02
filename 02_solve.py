2eec8f3aae411a12e43c42e47b4645cbf12bb576
Kenton Lam <kenton_lam@outlook.com>
2018-12-02 23:02:42 +1000

    Day 2 part 2.

--- 02_solve.py
+++ 02_solve.py
@@ -26,6 +26,27 @@ def solve_1(file_input: list) -> int:
         parts[1] += bool(flags & ChecksumFlags.EXACTLY_3)
     return parts[0] * parts[1]
 
+# unused
+def almost_equal(str1, str2):
+    assert len(str1) == len(str2)
+    differences = 0
+    str2_iter = iter(str2)
+    for c1 in str1:
+        if c1 != next(str2_iter):
+            differences += 1
+    return differences == 1
+
+# i'd say this is O(nm) in number of box IDs and length of each ID.
+def solve_2(file_input: iter) -> str:
+    seen = set() 
+    for word in file_input:
+        for i in range(len(word)):
+            word_part = word[:i] + word[i+1:] # delete char at i-th index
+            if word_part in seen:
+                return word_part
+            seen.add(word_part)
+
+
 if __name__ == "__main__":
     with open('02_input.txt') as f:
-        print(solve_1(f))
+        print(solve_2(f))
