4b4c03612c75d154eca8fd7bcd5b1a28d24addff
Kenton Lam <kenton_lam@outlook.com>
2018-12-11 21:23:10 +1000

    Template.

---
+++ dayZZ_solve.py
@@ -0,0 +1,18 @@
+from util import * 
+from collections import defaultdict, deque
+# import numpy as np 
+# import scipy as sp
+# from statistics import mean
+
+def parse(file):
+    pass
+
+def solve_1(data):
+    pass 
+
+def solve_2(data):
+    pass 
+
+if __name__ == "__main__":
+    with open('day12_input.txt') as f:
+        solve_1(parse(f))
